8e56ba0693bcd3f0bb9c00f811ed45eb8bf8b2ee
David Singh <davidnasingh@gmail.com>
2026-01-01 13:28:16 -0800

    add main sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='totalscreen'>
+      <div className='navbar'>
+        <div className='nav-left'>Los Angeles, CA</div>
+        <div className='nav-center'>Dawinder & Avneet</div>
+        <div className='nav-right'>10.10.26</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='main'>
+        <div className='main-left' style={{ backgroundImage: "url(/pic0.jpg)" }} />
+        <div className='main-right' style={{ backgroundImage: "url(/pic1.jpg)" }} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <div className='footer'>Details coming soon...</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
