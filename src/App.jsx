1bc57ffc2aa661fd0e9afafeb53e247a4a50011a
David Singh <davidnasingh@gmail.com>
2026-01-01 18:38:49 -0800

    compress images

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import './App.css';
+import PicOne from './assets/pic1.avif';
+import PicTwo from './assets/pic2.avif';
 
 function App() {
   return (
     <div className='totalscreen'>
       <div className='navbar'>
-        <div className='nav-left'>Los Angeles, CA</div>
         <div className='nav-center'>Dawinder & Avneet</div>
-        <div className='nav-right'>10.10.26</div>
+        <div className='nav-row'>
+          <div className='nav-left'>Los Angeles, CA</div>
+          <div className='nav-right'>10.10.26</div>
+        </div>
       </div>
       <div className='main'>
-        <div className='main-left' style={{ backgroundImage: "url(/pic0.jpg)" }} />
-        <div className='main-right' style={{ backgroundImage: "url(/pic1.jpg)" }} />
+        <div
+          className='main-left'
+          style={{ backgroundImage: `url(${PicOne})` }}
+        />
+        <div
+          className='main-right'
+          style={{ backgroundImage: `url(${PicTwo})` }}
+        />
       </div>
       <div className='footer'>Details coming soon...</div>
     </div>
